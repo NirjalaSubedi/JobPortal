07745b00119413338da2a1c27124d7468c41c062
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-02-25 18:40:46 +0545

    Implement user registration data extraction in userRegister function

--- backend/controllers/authControlers.js
+++ backend/controllers/authControlers.js
@@ -3,8 +3,8 @@ const bycrypt=require('bcryptjs');
 //Register User
 exports.userRegister=async(req,res)=> {
     try{
-        
+        const {name,email,password,role}=req.body;
     }catch(e){
-
+        res.status(200);
     }
 }
