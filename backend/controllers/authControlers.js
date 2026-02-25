5c68fbfe392d4a33d10624b807d6b857e582d046
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-02-25 18:46:59 +0545

    Fix bcrypt import typo and update error response status in userRegister function

--- backend/controllers/authControlers.js
+++ backend/controllers/authControlers.js
@@ -1,10 +1,15 @@
 const db= require('./config/db.js');
-const bycrypt=require('bcryptjs');
+const bcrypt=require('bcryptjs');
 //Register User
 exports.userRegister=async(req,res)=> {
     try{
         const {name,email,password,role}=req.body;
+        //password hasing
+        const salt= await bcrypt.genSalt(10);
+        const hashpassword= await bcrypt.hash(password,salt);
+
+
     }catch(e){
-        res.status(200);
+        res.status(500);
     }
 }
