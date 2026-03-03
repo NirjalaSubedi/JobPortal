7a21f3b98dc65c2fb95826da89ae02fc992153b6
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-03 20:51:54 +0545

    Add dotenv configuration to server initialization

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/db');
 const colors=require('colors');
+require('dotenv').config();
 const app=express();
 app.use(express.json());
 const checkconnection= async()=>{
