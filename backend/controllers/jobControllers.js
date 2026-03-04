8d7dcb5adfe3c3478fa48233fc644b44753aaf93
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-04 21:45:08 +0545

    feat: add job creation endpoint in jobControllers

---
+++ backend/controllers/jobControllers.js
@@ -0,0 +1,17 @@
+const db = require("../config/db");
+exports.createJob= async(req,res)=>{
+    try{
+       const {title,description,company,location,salary} = req.body;
+       const userid=req.user.id;
+       const [result]= await db.execute("INSERT INTO jobs(title,description,company,location,salary,createdBy) VALUES(?,?,?,?,?,?)",
+        [title,description,company,location,salary,userid])
+
+        res.status(201).json({
+            message:"job created success"
+        })
+    }catch(error){
+        res.status(500).json({
+            message:"job creation failed"
+        })
+    }
+}
