443b02f92a504411f51a70cb33f0036ce54c8991
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-04 09:33:28 +0545

    Add test-auth route with authmiddleware for token validation

--- backend/routes/authrouters.js
+++ backend/routes/authrouters.js
@@ -1,6 +1,13 @@
 const express= require('express');
+const authmiddleware=require('../middlewares/authmiddlewares');
 const router=express.Router();
 const {userRegister,loginUser}= require('../controllers/authControllers');
 router.post('/register', userRegister);
 router.post('/login',loginUser);
+router.get('/test-auth',authmiddleware=(req,res)=>{
+    res.status(200).json({
+        message:"token valid xa",
+        userdata:req.user
+    })
+})
 module.exports = router;
