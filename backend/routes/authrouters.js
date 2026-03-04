0295d99e104e519d5e9c8206d14b0972c61f5e6b
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-04 09:34:25 +0545

    Fix syntax error in test-auth route by correcting authmiddleware assignment

--- backend/routes/authrouters.js
+++ backend/routes/authrouters.js
@@ -4,7 +4,7 @@ const router=express.Router();
 const {userRegister,loginUser}= require('../controllers/authControllers');
 router.post('/register', userRegister);
 router.post('/login',loginUser);
-router.get('/test-auth',authmiddleware=(req,res)=>{
+router.get('/test-auth',authmiddleware,(req,res)=>{
     res.status(200).json({
         message:"token valid xa",
         userdata:req.user
