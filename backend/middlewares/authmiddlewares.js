e1e60c25a2a630f77f7b80e3de53b33505c7f28d
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-04 16:19:09 +0545

    Refactor authmiddleware to improve token extraction and error handling

--- backend/middlewares/authmiddlewares.js
+++ backend/middlewares/authmiddlewares.js
@@ -1,18 +1,20 @@
 const jwt=require('jsonwebtoken');
 const authmiddleware=async(req,res,next)=>{
     try{
-        const token=req.headers.authorization?.split("")[1];
-        if(!token){
-            res.status(401).json({
+        const authheader=req.headers.authorization;
+        if(!authheader||!authheader.startsWith("bearer")){ 
+            return res.status(401).json({
                 message:"No Token Authorization Denied "
             })
         }
+        const token=authheader?.split()[1];
         const decoded=jwt.verify(token,process.env.JWT_SECRET)
         user.req=decoded;
         next();
 
     }catch(error){
-        res.status(401).json({
+        console.log("JWT Error:", error.message);
+        return res.status(401).json({
             message:"Token not valid"
         })
     }
