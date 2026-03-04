dc0620b1f57fbd2f92f662a7c2e37411f576cd9a
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-04 09:22:39 +0545

    Add authmiddleware to handle JWT token validation

---
+++ backend/middlewares/authmiddlewares.js
@@ -0,0 +1,16 @@
+const jwt=require('jsonwebtoken');
+const authmiddleware=async(req,res,next)=>{
+    try{
+        const token=req.headers.authorization?.split("")[1];
+        if(!token){
+            res.status(401).json({
+                message:"No Token Authorization Denied "
+            })
+        }
+        const decoded=
+
+    }catch(error){
+
+    }
+
+}
