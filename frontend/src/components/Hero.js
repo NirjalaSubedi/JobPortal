92660a20576ebb695f84bfd3d45c23657b13ebab
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-07 19:06:16 +0545

    feat: enhance Hero component layout with styled div for job banner

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -2,8 +2,20 @@ import React from "react";
 import jobbanner from "../Assets/jobbanner.png";
 const Hero = ()=>{
     return(
-        <div>
-            <img src={jobbanner} alt="job banner"/>
+        <div style={{
+            width: '100%',
+            height: '400px',
+            overflow: 'hidden',
+            position: 'relative'
+        }}>
+            <img src={jobbanner} alt="job banner"
+            style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                filter: 'brightness(70%)'
+            }}
+            />
         </div>
     )
 }
