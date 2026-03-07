d3b8895962c39ac5fc4f392de4421306052785bd
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-07 19:02:38 +0545

    feat: add job banner image and integrate Hero component in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Hero/>
       <Routes>
         <Route path="/login" element={<Login />} />
        
