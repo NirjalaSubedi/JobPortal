dda8aff90656b47d4198ac3f55429541bf031a5f
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-08 09:15:57 +0545

    feat: add JobCategory component and include it in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Hero from './components/Hero';
+import JobCategory from './components/JobCategory'
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Hero/>
+      <JobCategory/>
+
       <Routes>
         <Route path="/login" element={<Login />} />
        
