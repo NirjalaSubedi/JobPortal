c3d61c58d118cb6b902bd1a267d322aa6e20f7f3
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-08 20:50:22 +0545

    feat: implement JobCategory component with initial styles and categories

--- frontend/src/components/JobCategory.js
+++ frontend/src/components/JobCategory.js
@@ -1,5 +1,16 @@
 const JobCategory = () => {
-  
+  const cats=['IT & Software','HealthCare','Banking','Design','marketing'];
+
+};
+const cardStyle = {
+  padding: '20px 40px',
+  backgroundColor: 'white',
+  borderRadius: '12px',
+  boxShadow: '0 4px 6px rgba(0,0,0,0.05)',
+  cursor: 'pointer',
+  transition: '0.3s',
+  fontWeight: '600',
+  color: '#3b82f6'
 };
 
 export default JobCategory;
