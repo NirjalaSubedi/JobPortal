919eaeee10e4362090d8f7ce4f7e784f02c76022
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-09 14:02:11 +0545

    feat: implement create company function with async handling and database insertion

--- backend/controllers/companyControllers.js
+++ backend/controllers/companyControllers.js
@@ -1,9 +1,11 @@
 const db= require ('../config/db');
-exports.company=(req,res)=>{
+exports.createcompany=async(req,res)=>{
     try{
-
+        const{name,logo_url,website,location}=req.body;
+        const [result]= await db.execute("NSERT INTO companies (name, logo_url, website, location, description) VALUES (?, ?, ?, ?, ?)",
+            [name, logo_url, website, location, description])
     }catch(error){
-        
+
     }
 
 }
