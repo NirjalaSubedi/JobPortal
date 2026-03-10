dcaaa22c03a4bd73c15fb8986e4c19df16f30e91
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-10 21:36:02 +0545

    feat: add fetchcompany route to authrouters

--- backend/routes/authrouters.js
+++ backend/routes/authrouters.js
@@ -3,7 +3,7 @@ const authmiddleware=require('../middlewares/authmiddlewares');
 const router=express.Router();
 const {userRegister,loginUser}= require('../controllers/authControllers');
 const {createJob,getalljobs,getsinglejob}=require('../controllers/jobControllers');
-const {createcompany}=require('../controllers/companyControllers');
+const {createcompany,fetchcompany}=require('../controllers/companyControllers');
 
 router.post('/register', userRegister);
 router.post('/login',loginUser);
@@ -20,4 +20,5 @@ router.post('/create-job',authmiddleware,createJob);
 router.get('/getjobs',getalljobs);
 router.get('/getsinglejob/:id',getsinglejob);
 router.post('/createcompany',createcompany);
+router.get('/fetchcompany',fetchcompany);
 module.exports = router;
