42c2c3d98955089835f6d6d158650b5c03b2976f
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-10 21:23:38 +0545

    feat: implement initial fetch logic in Companies component

--- frontend/src/components/Companies.js
+++ frontend/src/components/Companies.js
@@ -3,5 +3,13 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 const Companies=()=>{
     const {companies, setcompanies}=useState([]);
-    
+    useEffect(()=>{
+        const fetchcompany= async ()=>{
+            try{
+
+            }catch(error){
+                
+            }
+        }
+    })
 }
