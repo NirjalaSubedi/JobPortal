726f85a76890f1c056407be1a6c65c151578bb9f
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-10 21:21:16 +0545

    feat: initialize Companies component with state management and axios import

--- frontend/src/components/Companies.js
+++ frontend/src/components/Companies.js
@@ -1,1 +1,7 @@
 import react from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const Companies=()=>{
+    const {companies, setcompanies}=useState([]);
+    
+}
