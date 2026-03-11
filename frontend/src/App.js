6e53afbf5daaf67dcc3587ad5ee41330f95d8fc1
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-11 21:09:41 +0545

    feat: add Footer component and integrate it into App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import JobSection from './components/JobSection'; 
 import Companies from './components/Companies';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <JobSection/>
       <Companies/>
       <About/>
+      <Footer/>
 
       <Routes>
         <Route path="/login" element={<Login />} />
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from 'react-icons/fa';
+const Footer =()=>{
+    return(
+        <footer>
+            
+        </footer>
+    )
+}
+export default Footer;
