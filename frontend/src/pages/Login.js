c2481c6015db19d29b8e46d320212b14eb7d4b7d
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-12 18:52:23 +0545

    feat: add initial Login component with basic structure and styling

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import react from 'react';
+const Login = ()=>{
+    return(
+        <div className='Maincontainer'>
+            <div className="headerText">
+                <h1>sajilo<span className='highlight'>Rojgar</span></h1>
+                <p>Please login using the form below</p>
+            </div>
+
+        </div>
+    )
+}
+export default Login;
