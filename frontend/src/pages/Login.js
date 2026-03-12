fefcb32e66fb476d6cd58444ea645ac0cbd74103
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-12 18:59:21 +0545

    feat: add email and password input fields with remember me option to Login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -6,6 +6,19 @@ const Login = ()=>{
                 <h1>sajilo<span className='highlight'>Rojgar</span></h1>
                 <p>Please login using the form below</p>
             </div>
+            <div className='email'>
+                Email
+                <input type="email"/>
+            </div>
+            <div className='password'>
+                password
+                <input type="password"/>
+            </div>
+            <div className='rememberme'>
+                <input type="checkbox"/>
+                Remember Me
+            </div>
+            
 
         </div>
     )
