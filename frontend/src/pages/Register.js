13e9f8d1116e0b842f4f0d0cdc001294ac4c9f87
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-15 20:37:11 +0545

    feat: add header for user role selection in Register component

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,6 +5,8 @@ const Register = () => {
   const [role, setRole] = useState(null); 
 
   return (
+    <div>
+      <h1> SignUp As</h1>
     <div className='flex'>
       <div className=' flex flex-col items-center bg-green-100 p-4 m-4 border'>
         <User size={48} className="text-green-500 mb-4" />
@@ -14,6 +16,7 @@ const Register = () => {
         <Building2 size={48} className="text-red-500 mb-4" />
         <h1 className='text-red-600 font-bold'>Company</h1>
       </div>
+    </div>
     </div>
   );
 };
