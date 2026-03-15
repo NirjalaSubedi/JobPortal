da5ca6a131e07057ab23f7525db5fc3a82549b36
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-15 20:44:23 +0545

    feat: enhance styling of SignUp header in Register component

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -7,17 +7,17 @@ const Register = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-slate-50 font-sans p-4'>
       <h1 className='text-4xl font-black text-slate-800 mb-2'>Sajilo<span className='text-green-600'>Rojgar</span></h1>
-      <h3> SignUp As</h3>
-    <div className='flex'>
-      <div className=' flex flex-col items-center bg-green-100 p-4 m-4 border'>
-        <User size={48} className="text-green-500 mb-4" />
-        <h1 className='text-green-600 font-bold'>Candidate</h1>
+      <h3 className='text-slate-500 font-medium mb-10'> SignUp As</h3>
+      <div className='flex'>
+        <div className=' flex flex-col items-center bg-green-100 p-4 m-4 border'>
+          <User size={48} className="text-green-500 mb-4" />
+          <h1 className='text-green-600 font-bold'>Candidate</h1>
+        </div>
+        <div className=' flex flex-col items-center bg-red-100 p-4 m-4 border'>
+          <Building2 size={48} className="text-red-500 mb-4" />
+          <h1 className='text-red-600 font-bold'>Company</h1>
+        </div>
       </div>
-      <div className=' flex flex-col items-center bg-red-100 p-4 m-4 border'>
-        <Building2 size={48} className="text-red-500 mb-4" />
-        <h1 className='text-red-600 font-bold'>Company</h1>
-      </div>
-    </div>
     </div>
   );
 };
