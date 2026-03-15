9310946ee45cd0775fbe4a8bd355673e67c2925a
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-15 20:35:53 +0545

    feat: enhance layout and styling of Candidate and Company sections in Register component

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,14 +5,14 @@ const Register = () => {
   const [role, setRole] = useState(null); 
 
   return (
-    <div>
-      <div className='bg-green-100 p-4'>
+    <div className='flex'>
+      <div className=' flex flex-col items-center bg-green-100 p-4 m-4 border'>
         <User size={48} className="text-green-500 mb-4" />
-        <h1>Candidate</h1>
+        <h1 className='text-green-600 font-bold'>Candidate</h1>
       </div>
-      <div className='bg-red-100 p-4'>
+      <div className=' flex flex-col items-center bg-red-100 p-4 m-4 border'>
         <Building2 size={48} className="text-red-500 mb-4" />
-        <h1>Company</h1>
+        <h1 className='text-red-600 font-bold'>Company</h1>
       </div>
     </div>
   );
